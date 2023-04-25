e55adecc614f354b3a79b150895f3adb1ee16450
terurin <hamusutaateru@live.jp>
2023-04-26 05:21:17 +0900

    tokenize: add positioned integer's parser

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,79 @@
-use nom;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::*;
 use nom::combinator::*;
+use nom::error::convert_error;
 use nom::multi::many1;
 use nom::multi::*;
 use nom::*;
-use std::io::*;
+use nom::{self, error::Error};
+use nom_locate::{position, LocatedSpan};
 
-fn integer(s: &str) -> IResult<&str, i64> {
-    //let mut result=0;
+type Span<'a> = LocatedSpan<&'a str>;
+#[derive(Debug)]
+pub struct Token<'a> {
+    position: Span<'a>,
+    value: TokenValue,
+}
+#[derive(Debug)]
+pub enum TokenValue {
+    Int(i64),
+}
+
+fn integer(s: Span) -> IResult<Span, Token> {
+    let (s, pos) = position(s)?;
+
     let (s, sign) = opt(one_of("+-"))(s)?;
-    let (s, base) = map(
-        opt(alt((tag("0b"), tag("0q"), tag("0o"), tag("0d"), tag("0x")))),
-        |base| match base {
-            Some("0b") => 2,
-            Some("0q") => 4,
-            Some("0o") => 8,
-            Some("0d") | None => 10,
-            Some("0x") => 16,
-            Some(_) => unreachable!(),
-        },
-    )(s)?;
+    let (s, base) = opt(alt((tag("0b"), tag("0q"), tag("0q"), tag("0d"), tag("0x"))))(s)?;
+    let base = base.map_or(10, |b| match *b.fragment() {
+        "0b" => 2,
+        "0q" => 4,
+        "0o" => 8,
+        "0d" => 10,
+        "0x" => 16,
+        _ => unreachable!(),
+    });
 
     let (s, number) = map_opt(
-        separated_list1(many1(char('_')), many1(satisfy(|c: char| c.is_digit(base)))),
-        |digits| {
-            let mut result = 0i64;
-            if sign != Some('-') {
-                for digit in digits.into_iter().flatten() {
-                    result = result.checked_mul(base as i64)?;
-                    result = result.checked_add(digit.to_digit(16).unwrap() as i64)?;
-                }
+        recognize(separated_list1(
+            many1(tag("_")),
+            many1(satisfy(|c: char| c.is_digit(base))),
+        )),
+        |digits: Span| {
+            let digits = digits;
+            if sign == Some('-') {
+                digits
+                    .chars()
+                    .filter(|c| *c != '_')
+                    .try_fold(0i64, move |init, digit| {
+                        let digit = digit.to_digit(base)? as i64;
+                        Some(init.checked_mul(base as i64)?.checked_sub(digit)?)
+                    })
             } else {
-                for digit in digits.into_iter().flatten() {
-                    result = result.checked_mul(base as i64)?;
-                    result = result.checked_sub(digit.to_digit(16).unwrap() as i64)?;
-                }
+                digits
+                    .chars()
+                    .filter(|c| *c != '_')
+                    .try_fold(0i64, move |init, digit| {
+                        let digit = digit.to_digit(base)? as i64;
+                        Some(init.checked_mul(base as i64)?.checked_add(digit)?)
+                    })
             }
-            Some(result)
         },
     )(s)?;
-
-    Ok((s, number))
+    Ok((
+        s,
+        Token {
+            position: pos,
+            value: TokenValue::Int(number),
+        },
+    ))
 }
 
 fn main() {
-    let stdin = stdin();
+    let stdin = std::io::stdin();
     let mut line = String::new();
     while let Ok(_size) = stdin.read_line(&mut line) {
-        println!("{:?}", integer(&line));
+        println!("{:?}", integer(Span::new(&line)));
         line.clear();
     }
 }
